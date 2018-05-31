a098c6fcb1aeadf073bfaf2cc6e9f0888566df15
Michele Pesante <pesante.michele.96@gmail.com>
2018-05-31 11:17:01 +0200

    Prima versione, outline da sistemare

---
+++ Assets/Scripts/PawnController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class PawnController : MonoBehaviour {
+
+    private MeshRenderer childMeshRenderer;
+    private Outline childOutline;
+
+	// Use this for initialization
+	void Start () {
+        childMeshRenderer = GetComponentInChildren<MeshRenderer>();
+        childOutline = GetComponentInChildren<Outline>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnMouseEnter()
+    {
+        childOutline.eraseRenderer = false;
+    }
+
+    private void OnMouseExit()
+    {
+        childOutline.eraseRenderer = true;
+    }
+}
