fb7c18e750535efab83acff5597b2550427cd137
Michele Pesante <pesante.michele.96@gmail.com>
2018-05-31 19:43:03 +0200

    Aggiunta camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private RotationController cameraAnchor;
+
+    void Start()
+    {
+        cameraAnchor = FindObjectOfType<RotationController>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.LookAt(cameraAnchor.gameObject.transform);
+	}
+}
