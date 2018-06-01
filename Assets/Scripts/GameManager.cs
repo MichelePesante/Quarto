c6f890bb6e2ca40cb0a4f69257d96d62d05b0967
Michele Pesante <pesante.michele.96@gmail.com>
2018-06-01 02:09:44 +0200

    Aggiornati prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,72 @@ public class GameManager : MonoBehaviour {
 
     void Update()
     {
-
+        VictoryCondition();
     }
 
-    public void VictoryCheck() {
+    private void VictoryCondition()
+    {
+        foreach (PawnController pawn in PawnManager.Instance.GeneralPawns)
+        {
+            if (pawn.PawnCoordinates == CellCoordinates.X0_Y0) {
+                VictoryCheck(pawn, CellCoordinates.X0_Y1, CellCoordinates.X0_Y2, CellCoordinates.X0_Y3);
+            }
+            else if (pawn.PawnCoordinates == CellCoordinates.X3_Y0)
+            {
 
+            }
+            else if (pawn.PawnCoordinates == CellCoordinates.X0_Y3)
+            {
+
+            }
+            else if (pawn.PawnCoordinates == CellCoordinates.X3_Y0)
+            {
+
+            }
+            else if (pawn.PawnCoordinates == CellCoordinates.X1_Y0)
+            {
+
+            }
+            else if (pawn.PawnCoordinates == CellCoordinates.X2_Y0)
+            {
+
+            }
+            else if (pawn.PawnCoordinates == CellCoordinates.X0_Y1)
+            {
+
+            }
+            else if (pawn.PawnCoordinates == CellCoordinates.X0_Y2)
+            {
+
+            }
+        }
     }
+
+    private void VictoryCheck(PawnController firstVictoryPawn, CellCoordinates secondCoordinate, CellCoordinates thirdCoordinate, CellCoordinates fourthCoordinate)
+    {
+        PawnController secondVictoryPawn;
+        PawnController thirdictoryPawn;
+        PawnController fourthVictoryPawn;
+
+        foreach (PawnController pawn in PawnManager.Instance.GeneralPawns)
+        {
+           if (pawn.PawnCoordinates == secondCoordinate && pawn)
+            {
+                secondVictoryPawn = pawn;
+            }
+            else if (pawn.PawnCoordinates == thirdCoordinate && pawn)
+            {
+                thirdictoryPawn = pawn;
+            }
+            else if (pawn.PawnCoordinates == fourthCoordinate && pawn)
+            {
+                fourthVictoryPawn = pawn;
+            }
+        }
+
+        //if (firstVictoryPawn.isCircle == true && secondVictoryPawn.isCircle == true && thirdictoryPawn.isCircle == true && fourthVictoryPawn.isCircle == true) {
+        //
+        //}
+    }
+
 }
