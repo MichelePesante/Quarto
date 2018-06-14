fd7784c70ace245768cb25b65130d2d7ad8d7960
Michele Pesante <pesante.michele.96@gmail.com>
2018-06-14 12:43:23 +0200

    Rotazione camera funzionante

--- Assets/Scripts/RotationController.cs
+++ Assets/Scripts/RotationController.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class RotationController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float distance = 2f;
+    private float xSpeed = 90f;
+    private bool rightclicked = false;
+    private float x = 0f;
+
+    void Start ()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            rightclicked = true;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            rightclicked = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (rightclicked == true)
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            Quaternion rotation = Quaternion.Euler(0, x, 0);
+            transform.rotation = rotation;
+        }
+    }
+
 }
